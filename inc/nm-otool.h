d051ed677e1e14963a26de4e1ebb9f2de69ccdf1
Yanael Barbier <st3w4r@gmail.com>
2017-06-09 17:35:28 +0200

    Add header file

---
+++ inc/nm-otool.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   nm-otool.hh                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybarbier <ybarbier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/06/08 18:20:50 by ybarbier          #+#    #+#             */
+/*   Updated: 2017/06/08 18:23:12 by ybarbier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef NM_OTOOL_H
+# define NM_OTOOL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <mach-o/loader.h>
+#include <mach-o/nlist.h>
+
+
+#endif
