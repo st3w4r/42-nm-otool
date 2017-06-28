b87b11f0aab6071c39e3bd0e542cf37f19b8b57e
Yanael Barbier <st3w4r@gmail.com>
2017-06-28 20:13:31 +0200

    Split handler ar function

--- src/handler.c
+++ src/handler.c
@@ -155,77 +155,47 @@ void	handle_fat(s_file *file, s_format *format, void *ptr, bool is_64)
 	}
 }
 
+void *handle_ar_object(s_file *file, void *ptr, void *ranlib_arr, size_t i)
+{
+	void *ar_object;
+	struct ranlib *ranlib_elem;
+	struct ar_header *ar_header_elem;
+	size_t size_name;
+	char *name;
+
+
+	ranlib_elem = get_ranlib_element(ranlib_arr, i);
+	ar_header_elem = get_ar_header_element(ptr, ranlib_elem);
+	size_name = get_size_from_identifier((char*)ar_header_elem->file_identifier);
+	ar_object = get_ar_object(ptr, ranlib_elem, size_name);
+	name = get_ar_header_name(ar_header_elem, size_name);
+	file->sub_filename = name;
+	file->file_format = ARCHIVE;
+	file->is_displayed = FALSE;
+	return (ar_object);
+}
+
 /*
 ** Handle archive file
 */
 void	handle_ar(s_file *file, s_format *format, void *ptr, bool is_64)
 {
 	struct ar_header *ar_header;
-	struct ar_magic *ar_magic;
-	size_t size_name;
 	void *symdef;
-	uint32_t nranlibs;
 	void *ranlib_arr;
+	size_t size_name;
 	size_t i;
-	struct ranlib *ranlib_elem;
-	void *string_table;
-	char *string_elem;
-	struct ar_header *ar_header_elem;
-	void *ar_object;
-	char *name;
-	// s_archive_file_list **file_list;
 
-
-	ar_magic = get_ar_magic(ptr);
 	ar_header = get_ar_header(ptr);
-
 	size_name = get_size_from_identifier((char*)ar_header->file_identifier);
 	symdef = get_symdef(ar_header, size_name);
-	nranlibs = get_nranlibs(symdef);
 	ranlib_arr = get_ranlib_array(symdef);
-	string_table = get_ar_string_table(ranlib_arr, nranlibs);
-
-	// if ((file_list = malloc(sizeof(s_archive_file_list) * nranlibs)) == NULL)
-	// 	ft_malloc_error();
-
 	i = 0;
-	while (i < nranlibs)
+	while (i < get_nranlibs(symdef))
 	{
-		ranlib_elem = get_ranlib_element(ranlib_arr, i);
-		string_elem = get_ar_string_element(string_table, ranlib_elem);
-		ar_header_elem = get_ar_header_element(ptr, ranlib_elem);
-		size_name = get_size_from_identifier((char*)ar_header_elem->file_identifier);
-		ar_object = get_ar_object(ptr, ranlib_elem, size_name);
-		name = get_ar_header_name(ar_header_elem, size_name);
-
-		// ft_putendl(string_elem);
-		// ft_putstr(" ");
-		// ft_putstr("\n");
-		// ft_putstr(file->filename);
-		// ft_putstr("(");
-		// ft_putstr(name);
-		// ft_putstr("):\n");
-		// display_ar_header(file->filename, name);
-		// ft_putstr(string_elem);
-		// add_archive_list(format, ar_object, name);
-		file->sub_filename = name;
-		file->file_format = ARCHIVE;
-		file->is_displayed = FALSE;
-		handle_format(ar_object, file);
+		handle_format(handle_ar_object(file, ptr, ranlib_arr, i), file);
 		i++;
 	}
-	// s_archive_list * archive_list;
-	// archive_list = format->archive_list;
-	// while (archive_list != NULL)
-	// {
-	// 	ft_putstr("\n");
-	// 	ft_putstr(file->filename);
-	// 	ft_putstr("(");
-	// 	ft_putstr(archive_list->name);
-	// 	ft_putstr("):\n");
-	// 	handle_format(archive_list->ar_object, file);
-	// 	archive_list = archive_list->next;
-	// }
 }
 
 /*
